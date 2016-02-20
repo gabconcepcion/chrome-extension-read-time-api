47de0e882e16f147d6c103eb5ad1214ea55317a5
gabriel concepcion <ilovekhym@gmail.com>
2016-02-20 11:44:46 +0800

    * cleanup

--- onload.js
+++ onload.js
@@ -1,21 +1,3 @@
-
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-    callback(url);
-  });
-
-}
-
 function renderStatus(statusText) {
 	console.log(statusText);
     if(0==$('.apireadtimestatus').length)
